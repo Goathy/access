18370e318eb42e87235236a711786833321a4e0a
Jakub Gałecki <goathy.dev@gmail.com>
2023-06-25 16:58:37 +0200

    feat(UsersService): implement create method

---
+++ src/authorization/enums/permissions.enum.ts
@@ -0,0 +1,7 @@
+export enum Permissions {
+  ALL = 'ALL',
+  CREATE = 'CREATE',
+  READ = 'READ',
+  UPDATE = 'UPDATE',
+  DELETE = 'DELETE',
+}
--- src/users/services/users.service.spec.ts
+++ src/users/services/users.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Permissions } from '../../authorization/enums/permissions.enum';
+import { CreateUser } from '../dto/create-user.dto';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
@@ -14,5 +16,19 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create', () => {
+    const payload = new CreateUser(Permissions.ALL);
+
+    service.create(payload);
+
+    // @ts-expect-error access inmemory users store
+    const [exists] = Array.from(service.users.values());
+
+    expect(exists).toMatchObject({
+      id: expect.any(String),
+      permissions: ['ALL'],
+    });
   });
 });
